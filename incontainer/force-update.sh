5f748d02adf82ba118e5132b2922e07fc2e7213b
1thorsten <thorsten.winkler@online.de>
2021-03-11 23:44:05 +0100

    show http-over-all release

--- incontainer/force-update.sh
+++ incontainer/force-update.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 # curl -s --header "X-Debug-Out: something.meaningful" http://localhost:8338/force-update/
 # http://localhost:8338/force-update/
-# http://10.10.0.141:8338/force-update
-# http://10.20.1.122:8338/force-update
 
 SYS_ENV="/var/run/sys_env.sh"
 # shellcheck disable=SC1090
 if [ -e ${SYS_ENV} ]; then source "${SYS_ENV}"; fi
 
 source "/scripts/connect-services.sh"
+echo "$(date +'%T'): http-over-all -> RELEASE: ${RELEASE}"
 
 handle_update_jobs_lock "/var/run/force-update.lock" "handle-trap"
 
