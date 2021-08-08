23275c81a54c4882e4979609531d3d1a53b2ce1a
1thorsten <thorsten.winkler@online.de>
2021-08-08 19:54:54 +0200

    change signature decrypt_ext() and encrypt_ext()

--- incontainer/php/func_encrypt-ip.php
+++ incontainer/php/func_encrypt-ip.php
@@ -15,7 +15,7 @@ LOG::writeHost("func_encrypt-ip.php", $_REQUEST['remote_addr'], "ADDR: $remote_a
 $passphrase = strrev(str_replace('.', '', $remote_addr)) . KEY;
 $enc = $remote_addr;
 if (isset($_REQUEST['m'])) {
-    $enc = UnsafeCrypto::encrypt_ext($passphrase, $_REQUEST['m'], true);
+    $enc = UnsafeCrypto::encrypt_ext($passphrase,UnsafeCrypto::METHOD, $_REQUEST['m'], true);
 }
 
 echo $enc;
