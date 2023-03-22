4f5602e8ebc9a75fefc46b9b1760ef33cb1fe9fb
1thorsten <thorsten.winkler@online.de>
2023-03-22 07:42:18 +0100

    connected mode

--- incontainer/php/include/globals.php
+++ incontainer/php/include/globals.php
@@ -5,4 +5,5 @@
 # php -r "echo base64_encode(openssl_random_pseudo_bytes(32));" ; echo
 const KEY = '__CRYPT_KEY__';
 const FORCE_UPDATE_LOCK = '__FORCE_UPDATE_LOCK__';
+const CONNECTED_URLS = '__CONNECTED_URLS__';
 
