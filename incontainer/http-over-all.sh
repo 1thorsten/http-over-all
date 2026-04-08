d546e6185af43408324577f8754e026a53f8dd58
Thorsten Winkler <75699915+1thorsten@users.noreply.github.com>
2026-04-08 22:22:07 +0200

    feat(ftp): integrate mount_ftp_shares into startup sequence and add env example

--- incontainer/http-over-all.sh
+++ incontainer/http-over-all.sh
@@ -16,10 +16,10 @@ echo "$_ENV" | grep "^TZ" | awk -F '=' '{printf "export %s=\"%s\"\n",$1,$2 }'
 } > "${SYS_ENV}"
 
 echo "-- ENV --"
-for RES in DAV DOCKER GIT LOCAL NFS PROXY SMB SSH; do
+for RES in DAV DOCKER FTP GIT LOCAL NFS PROXY SMB SSH; do
   echo "$_ENV" | grep -vi "crypt" | grep "^${RES}_[0-9]*_" | sort -t '_' -k2 -n
 done
-for RES in DAV DOCKER GIT LOCAL NFS PROXY SMB SSH; do
+for RES in DAV DOCKER FTP GIT LOCAL NFS PROXY SMB SSH; do
   _ENV=$(echo "$_ENV" | grep -v "^${RES}_[0-9]*_")
 done
 echo "---------"
@@ -51,6 +51,12 @@ if ! mount_ssh_shares; then
 fi
 
 if ! mount_dav_shares; then
+  echo "$(date +'%T'): error connecting resource -> set to ERROR (http-over-all -> RELEASE: ${RELEASE})"
+  sleep inf
+  exit 1
+fi
+
+if ! mount_ftp_shares; then
   echo "$(date +'%T'): error connecting resource -> set to ERROR (http-over-all -> RELEASE: ${RELEASE})"
   sleep inf
   exit 1
